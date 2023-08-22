e6daaa54d65ad99c11cd4f46255ce1de8bfa1f8a
sujithvamshi <sujithvamshi123@gmail.com>
2023-08-22 10:14:31 +0530

    Update Tickets.js

--- client/src/components/Home/SupportTicket/Tickets.js
+++ client/src/components/Home/SupportTicket/Tickets.js
@@ -43,8 +43,8 @@ function Tickets({tickets}) {
             <div className="flex justify-between">
                 <Badge color={badges[ticket.status]}>{ticket.status}</Badge>
                 <Dropdown inline label="">
-                    <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                    href={"/tickets/"+ticket.id}>View Ticket</button>
+                    <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                    href={"/tickets/"+ticket.id}>View Ticket</a>
                     <button name={ticket.id} className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                     onClick={(e)=>{deleteTicket(e)}}>Delete</button>
                 </Dropdown>
