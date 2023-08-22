06dc4fe934e6002d78342ab5cc097f6c46b69745
sujithvamshi <sujithvamshi123@gmail.com>
2023-08-22 11:07:10 +0530

    V6

--- client/src/components/Home/SupportTicket/SupportTicket.js
+++ client/src/components/Home/SupportTicket/SupportTicket.js
@@ -7,6 +7,7 @@ import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import TicketView from "./TicketView";
 export default function SupportTicket() {
     const [tickets,setTickets] = useState([]);
+    const role = (localStorage.getItem('role').includes("USER"))?"USER":"ADMIN"
     const Fetching = useRef(false)
     const location = useLocation()
     const navigate = useNavigate();
@@ -35,10 +36,10 @@ export default function SupportTicket() {
           <Toast />
           <h1 className="mx-10 my-10 text-center text-2xl font-bold text-gray-900">
           Customer Support Tickets</h1>
-          <div className="flex flex-col items-center justify-center m-10">
+          {role=="USER" && <div className="flex flex-col items-center justify-center m-10">
             <button onClick={(e) => {navigate("/tickets/new")}} className="rounded-lg bg-indigo-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
                         Create New Ticket</button>
-          </div>
+          </div>}
           <div className="flex flex-wrap">
               {tickets.length>0 && <Tickets {...{tickets}} />}
           </div>
--- client/src/components/Home/SupportTicket/Tickets.js
+++ client/src/components/Home/SupportTicket/Tickets.js
@@ -5,6 +5,7 @@ import { AuthApi } from '../../common/Apis';
 function Tickets({tickets}) {
     const [id,setId] = useState(false);
     const navigate = useNavigate();
+    const role = (localStorage.getItem('role').includes("USER"))?"USER":"ADMIN"
     const handleProceed = () => {
         id && navigate(generatePath("/tickets/:id", { id }));
       };
@@ -39,14 +40,14 @@ function Tickets({tickets}) {
             ticket.subject = (ticket.subject.length > 30) ? ticket.subject.substring(0,29)+".....":ticket.subject
             ticket.description = (ticket.description.length > 100) ? ticket.description.substring(0,100)+".....":ticket.description
             return(
-        <Card className="mx-5 my-2.5 max-w-md">
+        <Card className="mx-5 my-2.5 max-w-md ">
             <div className="flex justify-between">
                 <Badge color={badges[ticket.status]}>{ticket.status}</Badge>
                 <Dropdown inline label="">
                     <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                     href={"/tickets/"+ticket.id}>View Ticket</a>
-                    <button name={ticket.id} className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                    onClick={(e)=>{deleteTicket(e)}}>Delete</button>
+                    {role=="USER" &&  <button name={ticket.id} className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={(e)=>{deleteTicket(e)}}>Delete</button>}
                 </Dropdown>
             </div>
             <div className="flex flex-col items-center">
@@ -56,12 +57,12 @@ function Tickets({tickets}) {
                 <span className="text-sm text-gray-500 dark:text-gray-400">
                     {ticket.description}
                 </span>
-                <div className="mt-4 flex space-x-3 lg:mt-6">
+                {role=="ADMIN" && <div className="mt-4 flex space-x-3 lg:mt-6">
                     <button onClick={(e) => {setId(ticket.id)}} className="inline-flex items-center rounded-lg bg-indigo-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
                         Update Status</button>
                     <button onClick={(e) => {setId(ticket.id)}} className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
                         Add Comment</button>
-                </div>
+                </div>}
             </div>
         </Card>
         )})}
